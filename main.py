5ce414967405bac3f25362d16d83f47081647f9f
Seungwoo <swoohan@kakao.com>
2021-10-13 07:33:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,39 @@ import ray
 from ray import tune
 from model_tf_manual import objective
 from ray.tune.schedulers.hb_bohb import HyperBandForBOHB
-from ray.tune.schedulers import ASHAScheduler
+from ray.tune.schedulers import ASHAScheduler, AsyncHyperBandScheduler
 from ray.tune.suggest.bohb import TuneBOHB
 
 bohb_seed =777
-
+epochs = 10
 def tune_mnist(training_iter):
 
+    # https://arxiv.org/abs/1810.05934
     sha_scheduler = ASHAScheduler(
-    max_t=10,  # 10 training iterations
+    max_t=epochs,  # 10 training iterations
     grace_period=1,
     reduction_factor=2)
 
+    # https://arxiv.org/abs/1603.06560
+    hyperband = AsyncHyperBandScheduler(
+        time_attr="training_iteration",
+        max_t=epochs,
+        reduction_factor=3,
+        brackets=1
+    )
+    # https://arxiv.org/abs/1807.01774
     bohb_hyperband = HyperBandForBOHB(
         time_attr="training_iteration",
-        max_t=81,
+        max_t=epochs,
         reduction_factor=3,
         stop_last_trials=False)
 
     bohb_search = TuneBOHB(
-        max_concurrent=4, seed=bohb_seed) # 병렬적으로 작동할 수
+        max_concurrent=4, # 병렬적으로 작동할 수
+        seed=bohb_seed) # 재현성을 위한 시드
 
     config = {
+        "training_iteration" : epochs,
         "activation": tune.choice(["relu", "tanh"]),
         "neuron1": tune.randint(32, 64),
         "neuron2": tune.randint(32, 64),
@@ -33,8 +44,8 @@ def tune_mnist(training_iter):
     analysis = tune.run(
         objective,
         config=config,
-        scheduler=sha_scheduler,
-        # search_alg=bohb_search,
+        scheduler=sha_scheduler, # or hyperband or bohb_hyperband
+        # search_alg=bohb_search, # 따로 설정하지 않을 시 랜덤 서치 및 그리드 서치 적용 
         num_samples=3, # 고려할 샘플 수들
         # stop={
         #     "mean_accuracy": 0.91
